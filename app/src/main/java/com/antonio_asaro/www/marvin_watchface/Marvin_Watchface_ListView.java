884de97df255a209d43c496b97e8642d6734a329
antonioasaro <antonio_asaro@hotmail.com>
2018-05-27 08:30:59 -0400

    Able to show icons + text

--- app/src/main/java/com/antonio_asaro/www/marvin_watchface/Marvin_Watchface_ListView.java
+++ app/src/main/java/com/antonio_asaro/www/marvin_watchface/Marvin_Watchface_ListView.java
@@ -1,5 +1,6 @@
 package com.antonio_asaro.www.marvin_watchface;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -7,10 +8,14 @@ import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.wearable.view.WearableListView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.Collections;
 import java.util.List;
@@ -30,12 +35,21 @@ public class Marvin_Watchface_ListView extends ListActivity implements
         main.addCategory(Intent.CATEGORY_LAUNCHER);
         List<ResolveInfo> launchables = pm.queryIntentActivities(main, 0);
         Collections.sort(launchables, new ResolveInfo.DisplayNameComparator(pm));
-////        for (ResolveInfo l : launchables) {
-////            Log.d(TAG, "Launchable: " + l.toString());
-////        }
-        String[] abc = {"abcabc", "defdef", "ijkijk", "nopnop", "pqrpqr", "rstrst", "tuvtuv", "xyzxyz"};
-        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_row, R.id.label, abc));
+
+        for (ResolveInfo l : launchables) {
+            Log.d(TAG, "Launchable: " + l.toString());
+        }
+        String[] abc = {"abcabc", "defdef", "ijkijk", "nopnop", "pqrpqr", "rstrst"};
+        int ijk = R.drawable.launcher_icon;
+        Integer[] def = {ijk, ijk, ijk, ijk, ijk, ijk};
+
+        CustomListAdapter adapter = new CustomListAdapter(this, abc, def);
         ListView lv = getListView();
+        lv.setAdapter(adapter);
+
+////        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_row, R.id.label, abc));
+////        this.setListAdapter(new ArrayAdapter<ImageView>(this, R.layout.list_row, R.id.icon, def));
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -62,4 +76,36 @@ public class Marvin_Watchface_ListView extends ListActivity implements
     public void onTopEmptyRegionClick() {
         Log.d(TAG, "onTopEmptyRegionClick");
     }
+}
+
+class CustomListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] itemname;
+    private final Integer[] imgid;
+    private static final String TAG = "Marvin_Watchface_Adapter";
+
+    public CustomListAdapter(Activity context, String[] itemname, Integer[] imgid) {
+        super(context, R.layout.list_main, itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.itemname=itemname;
+        this.imgid=imgid;
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.list_row, null,true);
+
+        Log.d(TAG, String.valueOf(position));
+        Log.d(TAG, itemname[position]);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.text);
+
+        imageView.setImageResource(imgid[position]);
+        txtTitle.setText(itemname[position]);
+        return rowView;
+
+    };
 }
