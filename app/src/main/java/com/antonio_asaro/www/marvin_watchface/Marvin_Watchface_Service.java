5617cf1b7db38da8d22dd6bab020742c4d4a0d00
antonioasaro <antonio_asaro@hotmail.com>
2018-07-24 06:06:04 -0400

    Brigthen the shading a tad

--- app/src/main/java/com/antonio_asaro/www/marvin_watchface/Marvin_Watchface_Service.java
+++ app/src/main/java/com/antonio_asaro/www/marvin_watchface/Marvin_Watchface_Service.java
@@ -436,7 +436,7 @@ public class Marvin_Watchface_Service extends CanvasWatchFaceService {
                 canvas.drawRect(0, 0, bounds.width(), bounds.height(), mBackgroundPaint);
                 Paint shading = new Paint();
                 for (int i=0; i<bounds.height()/4; i++) {
-                    shading.setARGB(0x40, 3*i/2, 3*i/2, 3*i/2);;
+                    shading.setARGB(0x48, 3*i/2, 3*i/2, 3*i/2);;
                     canvas.drawRect(0, 4*i, bounds.width(), 4*i+4, shading);
                 }
                 canvas.drawBitmap(mSpaceBitmap, 32, 36, null);
